7c5fbc908be51fbf2992998b48849015ee8cc76e
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-19 18:00:10 -0700

    using react devTools,eg component to show react tree

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import styles from './CourseInput.module.css';
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
 
 const CourseInput = (props) => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
@@ -26,7 +26,7 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div
-        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
@@ -37,3 +37,7 @@ const CourseInput = (props) => {
 };
 
 export default CourseInput;
+// if make changes in browsers input, it won't refelct to UI, because we don't feed back to feed the current value back into the input
+// solution: we can add value in line32  <input type="text" value=   onChange={goalInputChangeHandler} />
+// if add in the inspect , , it won't showed on input, but if click on add, It will add to the UI.
+//For existenig state items,even don't click on anything, still reflect changes on UI.
